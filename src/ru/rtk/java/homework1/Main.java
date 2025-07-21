0f560e5ba8d49c1f83c90b6c4ea95c49b9c6029a
Alexander Kudyakov <iopiopiop123450@gmail.com>
2025-07-21 10:47:15 +0300

    1

---
+++ src/ru/rtk/java/homework1/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package ru.rtk.java.homework1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите имя пользователя:");
+        String name = scanner.nextLine();
+        System.out.println("Привет," + name);
+    }
+}
